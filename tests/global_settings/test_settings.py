0d68baaf08137bd306a1a8d774195c64d44a0ffc
tarsil <tiago.arasilva@gmail.com>
2023-12-11 10:02:06 +0000

    Add extra settings test

--- tests/global_settings/test_settings.py
+++ tests/global_settings/test_settings.py
@@ -1,11 +1,12 @@
 from lilya.conf import settings
+from lilya.conf.enums import EnvironmentType
 from lilya.conf.global_settings import Settings
 
 
 def test_defaults():
     settings = Settings()
 
-    assert settings.debug == False
+    assert settings.debug is False
 
 
 def test_test_dict():
@@ -13,9 +14,10 @@ def test_test_dict():
 
     settings_dict = settings.dict()
 
-    assert settings_dict["debug"] == False
-    assert settings_dict["include_in_schema"] == True
+    assert settings_dict["debug"] is False
+    assert settings_dict["include_in_schema"] is True
 
 
-def xtest_conf_settings():
-    assert settings.debug == True
+def test_conf_settings():
+    assert settings.debug is True
+    assert settings.environment == EnvironmentType.TESTING
