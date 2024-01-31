1797e1874f083f2aaa224a207f6b272ed2509b36
tarsil <tiago.arasilva@gmail.com>
2024-01-31 17:15:29 +0000

    Add test for include()

--- lilya/core/urls.py
+++ lilya/core/urls.py
@@ -11,10 +11,8 @@ from lilya.exceptions import ImproperlyConfigured
 if TYPE_CHECKING:  # pragma: no cover
     from lilya.routing import BasePath
 
-DEFAULT_PATTERN = "route_patterns"
 
-
-def include(arg: Any, pattern: Optional[str] = settings.default_pattern) -> List["BasePath"]:
+def include(arg: Any, pattern: Optional[str] = settings.default_route_pattern) -> List["BasePath"]:
     """Simple retrieve functionality to make it easier to include
     routes in the urls. Example, nested routes.
     """
@@ -22,12 +20,11 @@ def include(arg: Any, pattern: Optional[str] = settings.default_pattern) -> List
         raise ImproperlyConfigured("The value should be a string with the format <module>.<file>")
 
     router_conf_module = import_module(arg)
-    pattern = pattern or DEFAULT_PATTERN
     patterns: List["BasePath"] = getattr(router_conf_module, pattern, None)
 
     assert (
         patterns is not None
     ), f"There is no pattern {pattern} found in {arg}. Are you sure you configured it correctly?"
 
-    assert not isinstance(patterns, list), f"{patterns} should be a list and not {type(patterns)}."
-    return patterns  # type: ignore
+    assert isinstance(patterns, list), f"{patterns} should be a list and not {type(patterns)}."
+    return patterns
---
+++ tests/test_urls_include.py
@@ -0,0 +1,45 @@
+import pytest
+
+from lilya.core.urls import include
+from lilya.exceptions import ImproperlyConfigured
+from lilya.routing import Path
+
+
+async def home() -> None:
+    """"""
+
+
+route_patterns = [Path(path="/home", handler=home)]
+
+
+def test_default_include_router_patterns():
+    include_routes = include("tests.test_urls_include")
+
+    assert len(include_routes) == 1
+
+
+my_urls = [Path(path="/home", handler=home)]
+
+
+def test_pattern_include():
+    include_routes = include("tests.test_urls_include", pattern="my_urls")
+
+    assert len(include_routes) == 1
+
+
+def test_raises_improperly_configured_for_arg():
+    with pytest.raises(ImproperlyConfigured):
+        include(1, pattern="my_urls")
+
+
+def test_raises_assertation_error_for_patterns():
+    with pytest.raises(AssertionError):
+        include("tests.test_urls_include", pattern="my_url_routes")
+
+
+my_url_routes_tuple = Path(path="/home", handler=home)
+
+
+def test_raises_assertation_error_for_patterns_not_list():
+    with pytest.raises(AssertionError):
+        include("tests.test_urls_include", pattern="my_url_routes_tuple")
