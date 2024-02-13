f7ccd94d95681a86d4cdab2421966690c5d10537
tarsil <tiago.arasilva@gmail.com>
2024-02-13 18:33:19 +0000

    Add Lifespan documentation

---
+++ docs_src/events/start_shutdown.py
@@ -0,0 +1,21 @@
+from saffier import Database, Registry
+
+from lilya.app import Lilya
+from lilya.requests import Request
+from lilya.routing import Path
+
+database = Database("postgresql+asyncpg://user:password@host:port/database")
+registry = Registry(database=database)
+
+
+async def create_user(request: Request) -> None:
+    # Logic to create the user
+    data = await request.json()
+    ...
+
+
+app = Lilya(
+    routes=[Path("/create", handler=create_user)],
+    on_startup=[database.connect],
+    on_shutdown=[database.disconnect],
+)
