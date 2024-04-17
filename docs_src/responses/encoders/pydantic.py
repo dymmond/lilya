4cc37b81b5a3d47f79d4eb52ae49e069587c1aca
tarsil <tiago.arasilva@gmail.com>
2024-04-17 17:37:03 +0100

    Fix typo in docs

--- docs_src/responses/encoders/pydantic.py
+++ docs_src/responses/encoders/pydantic.py
@@ -8,7 +8,7 @@ from lilya.encoders import Encoder, register_encoder
 
 
 class PydanticEncoder(Encoder):
-    __type__: BaseModel
+    __type__ = BaseModel
 
     def serialize(self, obj: BaseModel) -> dict[str, Any]:
         return obj.model_dump()
