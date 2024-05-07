97cdf3037f258c5a5000c97deb50ccc475112491
tarsil <tiago.arasilva@gmail.com>
2024-05-07 19:14:20 +0100

    Add environments docs

---
+++ docs_src/environments/normal.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from lilya.conf import Settings
+from lilya.environments import EnvironLoader
+
+loader = EnvironLoader()
+
+
+@dataclass
+class DatabaseSettings(Settings):
+    database_name: str = loader("DATABASE_NAME", cast=str, default="mydb")
+    database_user: str = loader("DATABASE_USER", cast=str, default="postgres")
+    database_password: str = loader("DATABASE_PASSWD", cast=str, default="postgres")
+    database_host: str = loader("DATABASE_HOST", cast=str, default="localhost")
+    database_port: int = loader("DATABASE_PORT", cast=int, default=5432)
+    api_key: str = loader("API_KEY", cast=str, default="")
