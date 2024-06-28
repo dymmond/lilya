752a58796f7f1f3412caf36a2752af3ff540cef8
Tiago Silva <tiago.arasilva@gmail.com>
2024-06-28 14:22:28 +0100

    Update context_vars.py

--- lilya/conf/context_vars.py
+++ lilya/conf/context_vars.py
@@ -6,15 +6,8 @@ OVERRIDE: ContextVar[bool] = ContextVar("override_settings", default=None)
 
 
 def get_override_settings() -> bool | None:
-    """
-    Gets the current active tenant in the context.
-    """
     return OVERRIDE.get()
 
 
 def set_override_settings(value: bool | None) -> None:
-    """
-    Sets the global tenant for the context of the queries.
-    When a global tenant is set the `get_context_schema` -> `SCHEMA` is ignored.
-    """
     OVERRIDE.set(value)
