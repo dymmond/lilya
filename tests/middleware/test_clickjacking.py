6b7807d16e1f15ee229ca39aaa3346a3f20249be
tarsil <tiago.arasilva@gmail.com>
2024-06-28 11:29:08 +0100

    Add test for defaults

--- tests/middleware/test_clickjacking.py
+++ tests/middleware/test_clickjacking.py
@@ -37,3 +37,20 @@ def test_xframe_options_same_origin_responses(test_client_factory):
     response = client.get("/")
 
     assert response.headers["x-frame-options"] == "SAMEORIGIN"
+
+
+@override_settings(x_frame_options=None)
+def test_xframe_options_defaults(test_client_factory):
+    def homepage():
+        return PlainText("Ok", status_code=200)
+
+    app = Lilya(
+        routes=[Path("/", handler=homepage)],
+        middleware=[DefineMiddleware(XFrameOptionsMiddleware)],
+    )
+
+    client = test_client_factory(app)
+
+    response = client.get("/")
+
+    assert response.headers["x-frame-options"] == "DENY"
