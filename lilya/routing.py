97afe9db004b490727e77711c31142a3c5837450
alex <devkral@web.de>
2025-01-02 13:10:46 +0100

    Revert "fix bug"
    This reverts commit b6bdba0b4c51e77d7909c65c28e22d37cab00707.

--- lilya/routing.py
+++ lilya/routing.py
@@ -775,7 +775,7 @@ class Host(BasePath):
         """
         Returns a list of declared path objects.
         """
-        return getattr(self.__base_app__, "routes", [])
+        return getattr(self.app, "routes", [])
 
     def search(self, scope: Scope) -> tuple[Match, Scope]:
         """
