41bb0cd0332682f472c9e6c31b8d845cc5585c47
tarsil <tiago.arasilva@gmail.com>
2025-06-04 16:09:27 +0200

    Beginning of OpenAPI docs [skip ci]

---
+++ lilya/contrib/openapi/openapi.py
@@ -0,0 +1,6 @@
+from typing import Any
+
+
+def spec(
+
+) -> Any: ...
---
+++ lilya/contrib/openapi/utils.py
@@ -0,0 +1,7 @@
+def get_openapi(app, path=None, **kwargs):
+    """
+    Generate OpenAPI schema for the given app.
+
+    Args:
+        app: The application instance.
+    """
