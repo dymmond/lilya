8f3ac1f59959e9b21d6dd57fa65bb75351c2daf5
Tiago Silva <tiago.arasilva@gmail.com>
2025-06-08 00:01:03 +0200

    Update msgspec.py [skip ci]

--- docs_src/encoders/msgspec.py
+++ docs_src/encoders/msgspec.py
@@ -3,17 +3,25 @@ from typing import Any
 import msgspec
 from msgspec import Struct
 
+from lilya._utils import is_class_and_subclass
 from lilya.encoders import Encoder, register_encoder
 
 
 class MsgSpecEncoder(Encoder):
-    __type__ = Struct
+
+    def is_type(self, value: Any) -> bool:
+        """
+        Check if the value is an instance of msgspec.Struct.
+
+        Args:
+            value (Any): The value to check.
+
+        Returns:
+            bool: True if the value is an instance of msgspec.Struct, False otherwise.
+        """
+        return isinstance(value, Struct) or is_class_and_subclass(value, Struct)
 
     def serialize(self, obj: Any) -> Any:
-        """
-        When a `msgspec.Struct` is serialised,
-        it will call this function.
-        """
         return msgspec.json.decode(msgspec.json.encode(obj))
 
     def encode(
@@ -22,7 +30,6 @@ class MsgSpecEncoder(Encoder):
         obj: Any,
     ) -> Any:
         return msgspec.json.decode(msgspec.json.encode(obj), type=structure)
-
 
 # A normal way
 register_encoder(MsgSpecEncoder())
