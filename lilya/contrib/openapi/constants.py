62a781f72cbdb5d7f356c43582e79683a94152a7
tarsil <tiago.arasilva@gmail.com>
2025-06-07 22:50:45 +0200

    Add request body

--- lilya/contrib/openapi/constants.py
+++ lilya/contrib/openapi/constants.py
@@ -1,3 +1,9 @@
 METHODS_WITH_BODY = {"GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"}
 REF_PREFIX = "#/components/schemas/"
 REF_TEMPLATE = "#/components/schemas/{model}"
+WRITING_METHODS = {"post", "put", "patch"}
+WRITING_STATUS_MAPPING = {
+    "post": 201,
+    "put": 200,
+    "patch": 200,
+}
