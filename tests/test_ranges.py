a88e1baf3f29e9cd039b2287fcf2d2f912c19cff
alex <devkral@web.de>
2025-07-04 17:06:36 +0200

    update tests

--- tests/test_ranges.py
+++ tests/test_ranges.py
@@ -12,6 +12,12 @@ from lilya.ranges import Range, parse_range_value
             "bytes=1,0-90,1, 10-20, 4-8", [Range(0, 90)], True, id="drop_overlapping_ranges"
         ),
         pytest.param(
+            "bytes=1, 10-20, 4-8",
+            [Range(1, 1), Range(4, 8), Range(10, 20)],
+            True,
+            id="ordering",
+        ),
+        pytest.param(
             "bytes=1, 4-8, 10-200",
             [Range(1, 1), Range(4, 8), Range(10, 100)],
             False,
@@ -56,3 +62,22 @@ def test_nomerge(byterange, expected, strict_fails):
             parse_range_value(byterange, 100, enforce_asc=True, merge_ranges=False).ranges
             == expected
         )
+
+
+@pytest.mark.parametrize(
+    "extra",
+    [
+        pytest.param({}, id="none"),
+        pytest.param({"merge_ranges": True}, id="merge"),
+        pytest.param({"merge_ranges": False}, id="nomerge"),
+    ],
+)
+def test_max_ranges_and_units(extra):
+    assert parse_range_value("bytes=1,2,3", max_values=100, **extra) is not None
+    assert parse_range_value("bytes=1,2,3", max_values=100, max_ranges=1, **extra) is None
+    # test other units
+    assert (
+        parse_range_value("unicorns=1,2,3", max_values={"unicorns": 100}, max_ranges=1, **extra)
+        is None
+    )
+    assert parse_range_value("bytes=1,2,3", max_values={"unicorns": 100}, **extra) is None
