05bfd6ef429680c9e26f020111d5a145eafc5e2f
tarsil <tiago.arasilva@gmail.com>
2025-07-21 18:20:51 +0200

    Complete open api iontegration with Controllers

---
+++ tests/openapi/controllers/test_responses_as_list.py
@@ -0,0 +1,132 @@
+from pydantic import BaseModel
+
+from lilya import __version__
+from lilya.contrib.openapi.datastructures import OpenAPIResponse
+from lilya.contrib.openapi.decorator import openapi
+from lilya.controllers import Controller
+from lilya.routing import Path
+from lilya.testclient import create_client
+
+
+class ErrorResponse(BaseModel):
+    detail: str
+    message: str
+
+
+class User(BaseModel):
+    name: str
+    age: int
+
+
+class UserController(Controller):
+    @openapi(
+        summary="Create a user",
+        description="Endpoint to create a user",
+        responses={
+            201: OpenAPIResponse(model=[User], description="User created successfully"),
+            400: OpenAPIResponse(model=[ErrorResponse], description="Bad Request"),
+        },
+    )
+    async def post(self, user: User):
+        return user
+
+
+def test_responses_decorator(test_client_factory):
+    with create_client(
+        routes=[
+            Path("/create", handler=UserController, methods=["POST"]),
+        ]
+    ) as client:
+        response = client.get("/openapi.json")
+
+        assert response.status_code == 200
+
+        assert response.json() == {
+            "openapi": "3.1.0",
+            "info": {
+                "title": "Lilya",
+                "version": __version__,
+                "summary": "Lilya application",
+                "description": "Yet another framework/toolkit that delivers.",
+                "contact": {
+                    "name": "Lilya",
+                    "url": "https://lilya.dev",
+                    "email": "admin@myapp.com",
+                },
+            },
+            "paths": {
+                "/create": {
+                    "post": {
+                        "operationId": None,
+                        "summary": "Create a user",
+                        "description": "Endpoint to create a user",
+                        "requestBody": {
+                            "content": {
+                                "application/json": {
+                                    "schema": [
+                                        {
+                                            "properties": {
+                                                "detail": {"title": "Detail", "type": "string"},
+                                                "message": {"title": "Message", "type": "string"},
+                                            },
+                                            "required": ["detail", "message"],
+                                            "title": "ErrorResponse",
+                                            "type": "object",
+                                        }
+                                    ]
+                                }
+                            }
+                        },
+                        "responses": {
+                            "201": {
+                                "description": "User created successfully",
+                                "content": {
+                                    "application/json": {
+                                        "schema": {
+                                            "type": "array",
+                                            "items": {"$ref": "#/components/schemas/User"},
+                                        }
+                                    }
+                                },
+                            },
+                            "400": {
+                                "description": "Bad Request",
+                                "content": {
+                                    "application/json": {
+                                        "schema": {
+                                            "type": "array",
+                                            "items": {
+                                                "$ref": "#/components/schemas/ErrorResponse"
+                                            },
+                                        }
+                                    }
+                                },
+                            },
+                        },
+                    }
+                }
+            },
+            "components": {
+                "schemas": {
+                    "ErrorResponse": {
+                        "properties": {
+                            "detail": {"title": "Detail", "type": "string"},
+                            "message": {"title": "Message", "type": "string"},
+                        },
+                        "required": ["detail", "message"],
+                        "title": "ErrorResponse",
+                        "type": "object",
+                    },
+                    "User": {
+                        "properties": {
+                            "name": {"title": "Name", "type": "string"},
+                            "age": {"title": "Age", "type": "integer"},
+                        },
+                        "required": ["name", "age"],
+                        "title": "User",
+                        "type": "object",
+                    },
+                }
+            },
+            "servers": [{"url": "/"}],
+        }
