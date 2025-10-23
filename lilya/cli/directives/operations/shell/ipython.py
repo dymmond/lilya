41a886b29cd7eb345779f43a9a27a98e4c677605
alex <devkral@web.de>
2025-10-23 12:59:07 +0200

    port edgy shell improvements to lilya

--- lilya/cli/directives/operations/shell/ipython.py
+++ lilya/cli/directives/operations/shell/ipython.py
@@ -1,6 +1,9 @@
+import asyncio
 import os
 import sys
 import typing
+
+from monkay.asgi import Lifespan
 
 from lilya.cli.directives.operations.shell.utils import import_objects
 from lilya.cli.terminal import Print
@@ -23,20 +26,31 @@ def get_ipython_arguments(options: typing.Any = None) -> typing.Any:
 def get_ipython(app: typing.Any, options: typing.Any = None) -> typing.Any:
     """Gets the IPython shell.
 
-    Loads the initial configurations from the main lilya settings
+    Loads the initial configurations from the main Edgy settings
     and boots up the kernel.
     """
     try:
-        from IPython import start_ipython
+        from IPython import start_ipython  # pyright: ignore[reportMissingModuleSource]
+        from IPython.core.async_helpers import (
+            get_asyncio_loop,  # pyright: ignore[reportMissingModuleSource]
+        )
 
         def run_ipython() -> None:
-            imported_objects = import_objects(app)
             ipython_arguments = get_ipython_arguments(options)
-            start_ipython(argv=ipython_arguments, user_ns=imported_objects)  # type: ignore
+            loop: asyncio.BaseEventLoop = get_asyncio_loop()  # type: ignore
+            ctx = Lifespan(app)
+            loop.run_until_complete(ctx.__aenter__())
+            try:
+                # we need an initialized registry first to detect reflected models
+                imported_objects: dict[str, typing.Any] = import_objects(app)
+                start_ipython(argv=ipython_arguments, user_ns=imported_objects)  # type: ignore
+            finally:
+                loop.run_until_complete(ctx.__aexit__())
 
-    except (ModuleNotFoundError, ImportError):
-        error = "You must have IPython installed to run this. Run `pip install ipython`"
-        printer.write_error(error)
+    except ImportError:
+        printer.write_error(
+            "You must have IPython installed to run this. Run `pip install ipython`."
+        )
         sys.exit(1)
 
     return run_ipython
